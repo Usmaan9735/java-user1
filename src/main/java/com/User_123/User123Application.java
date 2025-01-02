8a7fa2da1b857bd738cf2eded6d581bde4c19b08
Usmaan9735 <usaansyed8@gmail.com>
2025-01-02 09:07:28 +0530

    test1

---
+++ src/main/java/com/User_123/User123Application.java
@@ -0,0 +1,13 @@
+package com.User_123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(User123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/User_123/User123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.User_123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class User123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
